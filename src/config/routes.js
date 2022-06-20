cdd9a17b29a74669b78bfc7af1551261c27fabcd
Frederico Liborio <falos@iscte.pt>
2022-06-20 10:44:26 +0100

    backend config

---
+++ src/config/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+module.exports = function(server) {
+
+    //Definir URL base para todas as rotas
+    const router = express.Router()
+    //sempre que comecar por /api ele direciona para router
+    server.use('./api', router)
+
+    //Rotas de ciclo de pagamento
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(router, '/billingCycles')
+}
